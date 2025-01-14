b214defa76cc0d10cc137f50a7e1dba5b8b1b708
Vanilla M <bidisaaaa@gmail.com>
2025-01-14 21:24:01 +0530

    changes to navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
             <li><a href="#contact">Contact</a></li>
         </ul>
         <div>
-            <a href="#contact">Contact <Image src={assets.arrow_icon} className='w-3' alt=''/></a>
+            <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4'>Contact <Image src={assets.arrow_icon} className='w-3' alt=''/></a>
         </div>
     </nav>
     </>
