0eea33f33e9d8a7827eaac3a15036824db93adbf
Vanilla M <bidisaaaa@gmail.com>
2025-01-15 18:25:44 +0530

    Edited navbar and Tailwind config

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,19 +5,30 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
+    <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
+      <Image src={assets.header_bg_color} alt='' className='w-full'/>
+    </div>
+
     <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
         <a href="#top">
             <Image src={assets.logo} className='w-28 cursor-pointer mr-14' alt=''/>
         </a>
-        <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3'>
-            <li><a href="#top">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#services">Services</a></li>
-            <li><a href="#work">Work</a></li>
-            <li><a href="#contact">Contact</a></li>
+        <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50'>
+            <li><a className='font-Ovo' href="#top">Home</a></li>
+            <li><a className='font-Ovo' href="#about">About</a></li>
+            <li><a className='font-Ovo' href="#services">Services</a></li>
+            <li><a className='font-Ovo' href="#work">Work</a></li>
+            <li><a className='font-Ovo' href="#contact">Contact</a></li>
         </ul>
-        <div>
-            <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4'>Contact <Image src={assets.arrow_icon} className='w-3' alt=''/></a>
+        <div className='flex items-center gap-4'>
+
+            <button>
+              <Image src={assets.moon_icon} alt='' className='w-6'/>
+            </button>
+
+            <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo'>
+              Contact <Image src={assets.arrow_icon} className='w-3' alt=''/>
+            </a>
         </div>
     </nav>
     </>
