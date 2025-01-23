62bb501a6141158bfa345fb89dd08bebe036602f
Vanilla M <bidisaaaa@gmail.com>
2025-01-23 20:38:01 +0530

    Mounted the header to the pages tab.

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -20,8 +20,11 @@ const Header = () => {
         supernatural shadows, and emotional depth, unraveling secrets on a
         picturesque yet ominous island.
       </p>
-      <div>
-        <a href="#contact" className="px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2">
+      <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
+        <a
+          href="#contact"
+          className="px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2"
+        >
           contact me
           <Image src={assets.right_arrow_white} alt="" className="w-4" />
         </a>
