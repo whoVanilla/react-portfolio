8d6f252272afe9aa5252041a888ef3a56670039e
Vanilla M <bidisaaaa@gmail.com>
2025-01-23 20:17:39 +0530

    Added Header and it's styles.

---
+++ app/components/Header.jsx
@@ -0,0 +1,28 @@
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div>
+        <Image src={assets.profile_img} alt="" className="rounded-full w-32" />
+      </div>
+      <h3 className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo">
+        This is an Anime Project{" "}
+        <Image src={assets.hand_icon} alt="" className="w-6" />
+      </h3>
+      <h1 className="text-3xl sm:text-6xl lg:text-[66px] font-Ovo">
+        A time-warping based Sci-Fi Anime.
+      </h1>
+      <p className="max-w-2xl mx-auto font-Ovo">
+        Summer Time Rendering is a gripping mystery anime blending time loops,
+        supernatural shadows, and emotional depth, unraveling secrets on a
+        picturesque yet ominous island.
+      </p>
+      <div></div>
+    </div>
+  );
+};
+
+export default Header;
