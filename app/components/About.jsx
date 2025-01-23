7dfc829302d8dec602ae9e106d5f50417ae9e87f
Vanilla M <bidisaaaa@gmail.com>
2025-01-23 23:41:43 +0530

    Added smooth scrolling

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
+    <div id="about" className="w-full px-[12%] py-10 scroll-mt-20">
       <h4>Introduction</h4>
       <h2>About the anime</h2>
     </div>
