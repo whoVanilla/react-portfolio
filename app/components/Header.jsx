70f923b701edfa90f70b5a2e078975f9e521145c
Vanilla M <bidisaaaa@gmail.com>
2025-01-23 20:33:10 +0530

    added two buttons and their styles.

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -20,7 +20,20 @@ const Header = () => {
         supernatural shadows, and emotional depth, unraveling secrets on a
         picturesque yet ominous island.
       </p>
-      <div></div>
+      <div>
+        <a href="#contact" className="px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2">
+          contact me
+          <Image src={assets.right_arrow_white} alt="" className="w-4" />
+        </a>
+        <a
+          href="/sample-resume.pdf"
+          download
+          className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2"
+        >
+          my resume
+          <Image src={assets.download_icon} alt="" className="w-4" />
+        </a>
+      </div>
     </div>
   );
 };
