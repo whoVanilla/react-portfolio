fe6c5ec9a495bf5b86048098f2428d4a8002afba
Vanilla M <bidisaaaa@gmail.com>
2025-01-24 19:02:43 +0530

    Edits in about section

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -24,10 +24,13 @@ const About = () => {
           </p>
           <ul className="grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl">
             {infoList.map(({ icon, iconDark, title, description }, index) => (
-              <li key={index}>
-                <Image src={icon} alt={title} />
-                <h3>{title}</h3>
-                <p>{description}</p>
+              <li
+                key={index}
+                className="border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer"
+              >
+                <Image src={icon} alt={title} className="w-7 mt-3"/>
+                <h3 className="my-4 font-semibold text-gray-700">{title}</h3>
+                <p className="text-gray-600 text-sm">{description}</p>
               </li>
             ))}
           </ul>
