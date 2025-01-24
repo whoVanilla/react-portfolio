bf20f797371df41b3f4836f93affb589eaa7fadb
Vanilla M <bidisaaaa@gmail.com>
2025-01-24 19:19:26 +0530

    Tailwind config and about changes

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -26,7 +26,7 @@ const About = () => {
             {infoList.map(({ icon, iconDark, title, description }, index) => (
               <li
                 key={index}
-                className="border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500"
+                className="border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-black"
               >
                 <Image src={icon} alt={title} className="w-7 mt-3" />
                 <h3 className="my-4 font-semibold text-gray-700">{title}</h3>
