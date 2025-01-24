ceb7917da3ecfc58903e5daab83718e0a0c2e5f8
Vanilla M <bidisaaaa@gmail.com>
2025-01-24 17:58:54 +0530

    Edits in about section

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const About = () => {
   return (
     <div id="about" className="w-full px-[12%] py-10 scroll-mt-20">
-      <h4>Introduction</h4>
-      <h2>About the anime</h2>
+      <h4 className="text-center mb-2 text-lg font-Ovo">Introduction</h4>
+      <h2 className="text-center text-5xl font-Ovo">About the anime</h2>
     </div>
   );
 };
