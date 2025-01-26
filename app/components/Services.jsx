5e05f15449cb10269ab9663c5525c112d178032a
Vanilla M <bidisaaaa@gmail.com>
2025-01-26 21:52:29 +0530

    Changes in services

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -1,10 +1,25 @@
+import { serviceData } from "@/assets/assets";
+import Image from "next/image";
 import React from "react";
 
 const Services = () => {
   return (
     <div id="services" className="w-full px-[12%] py-10 scroll-mt-20">
-      <h4 className="text-center mb-2 text-lg font-Ovo">Introduction</h4>
-      <h2 className="text-center text-5xl font-Ovo">About the anime</h2>
+      <h4 className="text-center mb-2 text-lg font-Ovo">What I offer</h4>
+      <h2 className="text-center text-5xl font-Ovo">My services</h2>
+
+      <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">
+        Shinpei Ajiro possesses the unique ability to relive time loops,
+        allowing him to uncover secrets, strategize, and protect his loved ones.
+      </p>
+      <div className="grid grid-cols-4 gap-6 my-10">
+        {serviceData.map(({ icon, title, description, link }, index) => (
+          <div key={index}>
+            <Image src={icon} alt="" className="w-10"/>
+            <h3></h3>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
