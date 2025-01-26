6325936c157c70f84ba9d54ccd1c2e77532542d5
Vanilla M <bidisaaaa@gmail.com>
2025-01-26 22:15:14 +0530

    Edits in services

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -14,7 +14,10 @@ const Services = () => {
       </p>
       <div className="grid grid-cols-4 gap-6 my-10">
         {serviceData.map(({ icon, title, description, link }, index) => (
-          <div key={index}>
+          <div
+            key={index}
+            className="border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500"
+          >
             <Image src={icon} alt="" className="w-10" />
             <h3 className="text-lg my-4 text-gray-700">{title}</h3>
             <p className="text-sm text-gray-600 leading-5">{description}</p>
