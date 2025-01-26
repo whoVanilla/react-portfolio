9d0c51e89c2981165df935138e6a82523e9be249
Vanilla M <bidisaaaa@gmail.com>
2025-01-26 19:42:33 +0530

    Mounted the services section to page

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Services = () => {
   return (
-    <div></div>
-  )
-}
+    <div id="services" className="w-full px-[12%] py-10 scroll-mt-20">
+      <h4 className="text-center mb-2 text-lg font-Ovo">Introduction</h4>
+      <h2 className="text-center text-5xl font-Ovo">About the anime</h2>
+    </div>
+  );
+};
 
-export default Services
+export default Services;
