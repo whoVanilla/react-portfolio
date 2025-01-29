6f9cd324db8f28506fe0f1047ae3199f06d33b5b
Vanilla M <bidisaaaa@gmail.com>
2025-01-29 19:34:09 +0530

    Changes on work component

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Work = () => {
   return (
-    <div></div>
-  )
-}
+    <div id="work" className="w-full px-[12%] py-10 scroll-mt-20">
+      <h4 className="text-center mb-2 text-lg font-Ovo">My portfolio</h4>
+      <h2 className="text-center text-5xl font-Ovo">My latest work</h2>
 
-export default Work
+      <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">
+        Shinpei Ajiro possesses the unique ability to relive time loops,
+        allowing him to uncover secrets, strategize, and protect his loved ones.
+      </p>
+    </div>
+  );
+};
+
+export default Work;
