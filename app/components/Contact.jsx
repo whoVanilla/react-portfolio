4071968b21b698d9a3ac0effa7ced5ec61321eae
Vanilla M <bidisaaaa@gmail.com>
2025-02-07 19:30:19 +0530

    Mounted Contact component on pages and edited contact component

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
-    <div id='contact' className="w-full px-[12%] py-10 scroll-mt-20">
-        <h4 className="text-center mb-2 text-lg font-Ovo">My portfolio</h4>
-      <h2 className="text-center text-5xl font-Ovo">My latest work</h2>
+    <div
+      id="contact"
+      className="w-full px-[12%] py-10 scroll-mt-20 bg-[url('/footer-bg-color.png')] bg-no-repeat bg-center bg-[length:90%_auto]"
+    >
+      <h4 className="text-center mb-2 text-lg font-Ovo">Connect with me</h4>
+      <h2 className="text-center text-5xl font-Ovo">Get in touch</h2>
 
       <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">
         Shinpei Ajiro possesses the unique ability to relive time loops,
         allowing him to uncover secrets, strategize, and protect his loved ones.
       </p>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
