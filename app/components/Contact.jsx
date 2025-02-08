037b28c5a254ecb31890d0e5b63a299f41d4c4a7
Vanilla M <bidisaaaa@gmail.com>
2025-02-08 21:28:47 +0530

    comment commit

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
 
       <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">
         Shinpei Ajiro possesses the unique ability to relive time loops,
-        allowing him to uncover secrets, strategize,, and protect his loved ones.
+        allowing him to uncover secrets, strategize, and protect his loved ones.
       </p>
 
       <form className="max-w-2xl mx-auto">
