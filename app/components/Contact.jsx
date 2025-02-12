31f81b8c39a7a60bb0bb495612d97673b49eba78
Vanilla M <bidisaaaa@gmail.com>
2025-02-12 21:45:04 +0530

    changes in contact section

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,3 +1,4 @@
+import { assets } from "@/assets/assets";
 import React from "react";
 
 const Contact = () => {
@@ -16,15 +17,32 @@ const Contact = () => {
 
       <form className="max-w-2xl mx-auto">
         <div className="grid grid-cols-auto gap-6 mt-10 mb-8">
-          <input type="text" placeholder="Enter your name" required 
-          className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white"/>
-          <input type="email" placeholder="Enter your email" required 
-          className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white"/>
+          <input
+            type="text"
+            placeholder="Enter your name"
+            required
+            className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white"
+          />
+          <input
+            type="email"
+            placeholder="Enter your email"
+            required
+            className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white"
+          />
         </div>
-        <textarea rows="6" placeholder="Enter your message" required
-        className="w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6"></textarea>
+        <textarea
+          rows="6"
+          placeholder="Enter your message"
+          required
+          className="w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6"
+        ></textarea>
 
-        <button type="submit">Submit</button>
+        <button
+          type="submit"
+          className="py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500"
+        >
+          Submit now <Image src={assets.right_arrow_white} alt='' className='w-4' />
+        </button>
       </form>
     </div>
   );
