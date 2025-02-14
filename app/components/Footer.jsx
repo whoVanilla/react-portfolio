13bc33abb576b8a8e0fbe351a7e8c32af0be0ce1
Vanilla M <bidisaaaa@gmail.com>
2025-02-15 01:08:02 +0530

    edits in footer component

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,9 +1,13 @@
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-        
+    <div className='mt-20'>
+        <div className='text-center'>
+            <Image src={assets.logo} alt='' className='w-36 mx-auto mb-2'/>
+        </div>
     </div>
   )
 }
